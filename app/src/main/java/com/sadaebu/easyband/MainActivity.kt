1e358647f5ea71e5fdc623ea93c496e4c7d43ec4
Jang Eunsun <20211234@duksung.ac.kr>
2024-02-07 19:45:12 +0900

    ✨ [main] 로그인 버튼 클릭 이벤트 추가

--- app/src/main/java/com/sadaebu/easyband/MainActivity.kt
+++ app/src/main/java/com/sadaebu/easyband/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.sadaebu.easyband
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 로그인 화면으로 이동
+        val goLogin = findViewById<Button>(R.id.main_login)
+        goLogin.setOnClickListener {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
