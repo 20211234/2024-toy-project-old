887602d60300043f2abf798524e0dc138c8b5bdb
Jang Eunsun <20211234@duksung.ac.kr>
2024-02-12 14:19:51 +0900

    ✨ [main] 회원가입 버튼 클릭 이벤트 추가

--- app/src/main/java/com/sadaebu/easyband/MainActivity.kt
+++ app/src/main/java/com/sadaebu/easyband/MainActivity.kt
@@ -16,5 +16,13 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        // 회원가입 화면으로 이동
+        val goSignUp = findViewById<Button>(R.id.main_signup)
+        goSignUp.setOnClickListener {
+            val intent = Intent(this@MainActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
