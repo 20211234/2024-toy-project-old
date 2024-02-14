fea58d8123f9a5a00c56d6aff81e3e277f1c1b6b
Jang Eunsun <20211234@duksung.ac.kr>
2024-02-14 11:21:03 +0900

    ✨ [profile] 저장 버튼 클릭 이벤트 추가

--- app/src/main/java/com/sadaebu/easyband/ProfileActivity.kt
+++ app/src/main/java/com/sadaebu/easyband/ProfileActivity.kt
@@ -1,11 +1,20 @@
 package com.sadaebu.easyband
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        // 홈 화면으로 이동
+        val goMyBands = findViewById<Button>(R.id.profile_save)
+        goMyBands.setOnClickListener {
+            val intent = Intent(this@ProfileActivity, MyBandsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
