d18d878d18defe1784a0851303205e90553a9eb9
Jang Eunsun <20211234@duksung.ac.kr>
2024-02-14 13:26:25 +0900

    ✨ [signup] 확인 버튼 클릭 이벤트 추가

--- app/src/main/java/com/sadaebu/easyband/SignUpActivity.kt
+++ app/src/main/java/com/sadaebu/easyband/SignUpActivity.kt
@@ -1,11 +1,20 @@
 package com.sadaebu.easyband
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        // 프로필 화면으로 이동
+        val goProfile = findViewById<Button>(R.id.signup_enter)
+        goProfile.setOnClickListener {
+            val intent = Intent(this@SignUpActivity, ProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
